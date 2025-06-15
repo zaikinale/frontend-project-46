145c7816835de3cf2136c9a9f773dfee82753496
Alexei Zaikin <zaikinaleks04@mail.ru>
2025-06-15 16:41:46 +0300

    2_step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+const { version } = require('../package.json');
 import { program } from 'commander';
+
+// const format = (types) => {
+//     const incomingType = [...types]
+// };
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
+  .version(version, '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'display help for command');
 
 program.parse(process.argv);
